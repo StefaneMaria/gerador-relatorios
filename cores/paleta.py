ca7a79e8e9adddfde56f8f67a4246a49eaf424f4
Stefane Maria <stefane.maria0901@gmail.com>
2026-06-15 14:31:10 -0300

    feat(paleta): adiciona método de conversão de hex para RGB para manipulação de cores

--- cores/paleta.py
+++ cores/paleta.py
@@ -17,3 +17,11 @@ class CorHex:
     GRADE_SUAVE     = "#EEF1F5"
     BADGE_FUNDO     = "#FFF8EC"
     BARRA_PADRAO    = "#4A90D9"
+    
+    def hex_para_rgb(hex_color: str) -> tuple:
+        """
+       Converte cor hexadecimal para tupla RGB no intervlo 0-1 (matplotlib).
+        """
+        
+        hex_limpo = hex_color.lstrip('#')
+        return tuple(int(hex_limpo[i:i+2], 16) / 255 for i in (0, 2, 4))
