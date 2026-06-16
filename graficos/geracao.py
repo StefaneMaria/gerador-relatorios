c3de49fe93214582895013163bb5c3cf02537c43
Stefane Maria <stefane.maria0901@gmail.com>
2026-06-16 16:14:43 -0300

    feat(graphics): implementa função de configuração de eixos para gráficos

---
+++ graficos/geracao.py
@@ -0,0 +1,8 @@
+from cores.paleta import CorHex
+
+def _configurar_eixos(ax, ocultar_bordas=("top", "right")):
+    ax.spines[list(ocultar_bordas)].set_visible(False)
+    ax.spines[["left", "bottom"]].set_color(CorHex.BORDA_SUAVE)
+    ax.yaxis.grid(True, color=CorHex.GRADE_SUAVE, zorder=0)
+    ax.set_axisbelow(True)
+    
