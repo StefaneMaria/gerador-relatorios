0f593b40c4738f9222d5249080630cce58d94234
Stefane Maria <stefane.maria0901@gmail.com>
2026-06-16 16:19:05 -0300

    feat(graphics):adiciona função para salvar figura em buffer e atualiza requirements

--- graficos/geracao.py
+++ graficos/geracao.py
@@ -1,3 +1,7 @@
+import io
+
+import matplotlib.pyplot as plt
+
 from cores.paleta import CorHex
 
 def _configurar_eixos(ax, ocultar_bordas=("top", "right")):
@@ -9,3 +13,11 @@ def _configurar_eixos(ax, ocultar_bordas=("top", "right")):
 def _aplicar_fundo_branco(fig, ax):
     fig.patch.set_facecolor(CorHex.BRANCO)
     ax.set_facecolor(CorHex.BRANCO)
+
+def _salvar_figura_em_buffer(fig) -> io.BytesIO:
+    buffer = io.BytesIO()
+    fig.savefig(buffer, format='png', dpi=150, bbox_inches='tight')
+    plt.close(fig)
+    buffer.seek(0)
+    return buffer
+    
