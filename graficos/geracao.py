6b7d3fd29adbf60a561db873995db8f919a8f03e
Stefane Maria <stefane.maria0901@gmail.com>
2026-06-16 16:15:29 -0300

    feat(graphics): implementa função para aplicar fundo branco em gráficos

--- graficos/geracao.py
+++ graficos/geracao.py
@@ -6,3 +6,6 @@ def _configurar_eixos(ax, ocultar_bordas=("top", "right")):
     ax.yaxis.grid(True, color=CorHex.GRADE_SUAVE, zorder=0)
     ax.set_axisbelow(True)
     
+def _aplicar_fundo_branco(fig, ax):
+    fig.patch.set_facecolor(CorHex.BRANCO)
+    ax.set_facecolor(CorHex.BRANCO)
